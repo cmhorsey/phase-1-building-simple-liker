269c2e9c35bcfb7d38001bc2a23c1edf9c95e9f4
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-17 15:14:36 -0400

    Add reject response

--- main.js
+++ main.js
@@ -4,9 +4,54 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Test server function
+// Create click event for heart button
+// Console.log based on server function response
+
+document.addEventListener('DOMContentLoaded', function() {
 
 
+  const mediaPost = document.querySelectorAll('.media-post')
+  // console.log(mediaPost)
 
+  // const heartBtn = document.querySelectorAll('.like-glyph')
+  // // console.log(heartBtn)
+  // for(let item of heartBtn) {
+  //   item.addEventListener('click', () => {
+  //     mimicServerCall()
+  //     .then(handleResolve)
+  //     .catch(handleError)
+  //   })
+  // }
+
+  let span = document.querySelectorAll('span.like-glyph')
+  span.forEach(item => {
+    item.addEventListener('click', () => {
+      mimicServerCall()
+      .then(handleResolve)
+      .catch(handleError)
+    })
+  })
+
+  function handleResolve(resolve){
+    console.log(resolve)
+    }
+
+  let modal = document.getElementById('modal')
+  let modalMessage = document.getElementById('modal-message')
+
+  function handleError(reject) {
+    modal.classList.remove('hidden')
+    modalMessage.innerHTML = reject
+  }
+
+
+  
+
+});
+
+
+// heartBtn.addEventListener('click', console.log('liked'))
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
